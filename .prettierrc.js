5f10c71c774db96b44d7a17ad594c7ebae7d8a25
peris <964480899@qq.com>
2022-06-15 20:03:56 +0800

    'project-init'

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  /* 使用单引号包含字符串 */
+  singleQuote: true,
+  /* 不添加行尾分号 */
+  semi: false,
+  /* 在对象属性添加空格 */
+  bracketSpacing: true,
+  /* 优化html闭合标签不换行的问题 */
+  htmlWhitespaceSensitivity: 'ignore',
+  trailingComma: 'none',
+  endOfLine: 'auto'
+}
