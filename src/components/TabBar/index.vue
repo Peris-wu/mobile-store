1e6348f05cdc169af4d3b5dd84207a3118816e1a
peris <964480899@qq.com>
2022-06-22 05:01:15 +0800

    首页 header header-nav swipe 自定义indictors

---
+++ src/components/TabBar/index.vue
@@ -0,0 +1,79 @@
+<!--  -->
+<template>
+  <div class="tab-bar-wrap">
+    <ul>
+      <li
+        v-for="(iconItem, index) in tabBarList"
+        @click="navigateTo(iconItem.path)"
+        :key="index"
+        :style="{ color: iconItem.path === $route.path ? 'red' : '' }"
+      >
+        <i :class="['iconfont', ...iconItem.className]"></i>
+        <span>{{ iconItem.text }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabBar',
+  data() {
+    this.tabBarList = [
+      {
+        text: '首页',
+        className: ['icon-home'],
+        path: '/'
+      },
+      {
+        text: '分类',
+        className: ['icon-category'],
+        path: '/category'
+      },
+      {
+        text: '购物车',
+        className: ['icon-cart'],
+        path: '/cart'
+      },
+      {
+        text: '我的',
+        className: ['icon-my'],
+        path: '/my'
+      }
+    ]
+    return {}
+  },
+  components: {},
+  methods: {
+    navigateTo(path) {
+      let routeInfo = this.$route
+      if (path === routeInfo.path) return
+      this.$router.replace({
+        path
+      })
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.tab-bar-wrap {
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  z-index: 999;
+  ul {
+    display: flex;
+    justify-content: space-around;
+    li {
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      align-items: center;
+      flex: 1;
+      span {
+        font-size: 14px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/Category/index.vue
@@ -0,0 +1,19 @@
+<!--  -->
+<template>
+  <div>
+    <tab-bar></tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from '@/components/TabBar'
+export default {
+  name: 'CategoryView',
+  data() {
+    return {}
+  },
+
+  components: { TabBar }
+}
+</script>
+<style lang="scss" scoped></style>
