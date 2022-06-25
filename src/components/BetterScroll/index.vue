17f10afdac0b51f566112ba145754e7ac09fb83a
peris <964480899@qq.com>
2022-06-26 01:53:13 +0800

    首页（小七推荐 专题优选 爆款推荐 茶文化）

---
+++ src/components/BetterScroll/index.vue
@@ -0,0 +1,68 @@
+<!--  -->
+<template>
+  <div
+    ref="bsRef"
+    class="bscroll-wrap"
+    :style="{
+      width: width ? `${width}px` : '100%',
+      height: height ? `${height}px` : '100%'
+    }"
+  >
+    <div
+      :style="{
+        display: isScrollX ? 'inline-flex' : ''
+      }"
+    >
+      <slot name="bScroll"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from '@better-scroll/core'
+export default {
+  name: 'BetterScroll',
+  props: {
+    width: {
+      type: String,
+      default: ''
+    },
+    height: {
+      type: String,
+      default: ''
+    },
+    isScrollX: {
+      type: Boolean,
+      default: false
+    },
+    isScrollY: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      bs: null
+    }
+  },
+
+  components: {},
+  mounted() {
+    this.bs = new BScroll(this.$refs.bsRef, {
+      scrollX: this.isScrollX,
+      scrollY: this.isScrollY
+    })
+  },
+  destroyed() {
+    this.bs.destroy()
+  }
+}
+</script>
+<style lang="scss" scoped>
+.bscroll-wrap {
+  overflow: hidden;
+  img {
+    width: 100%;
+  }
+}
+</style>
---
+++ src/components/OverHidden/index.vue
@@ -0,0 +1,32 @@
+<!--  -->
+<template>
+  <div class="overhidden" :style="{ '-webkit-line-clamp': lineClamp }">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OverHidden',
+  props: {
+    lineClamp: {
+      type: Number,
+      default: 1
+    }
+  },
+  data() {
+    return {}
+  },
+
+  components: {}
+}
+</script>
+<style lang="scss" scoped>
+.overhidden {
+  display: -webkit-box;
+  width: 100%;
+  overflow: hidden;
+  -webkit-box-orient: vertical;
+  word-wrap: break-word;
+}
+</style>
