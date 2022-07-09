ac53a0d88ad1cffecf6e328db8674b544db4a93c
peris <964480899@qq.com>
2022-07-10 03:50:48 +0800

    地址编辑静态页面完成

---
+++ src/components/AllHeader/index.vue
@@ -0,0 +1,58 @@
+<!--  -->
+<template>
+  <header>
+    <div class="left" :style="{ width: `${leftWidth}px` }">
+      <slot name="left">
+        <i class="iconfont icon-xiaoyuhao"></i>
+      </slot>
+    </div>
+    <div class="center">
+      <slot name="center"></slot>
+    </div>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AllHeader',
+  props: {
+    leftWidth: {
+      type: Number || String,
+      default: 44
+    }
+  },
+  data() {
+    return {}
+  },
+
+  components: {}
+}
+</script>
+<style lang="scss" scoped>
+header {
+  display: flex;
+  background-color: #b0352f;
+  .left {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 44px;
+  }
+  .center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+  }
+  .right {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 44px;
+    height: 44px;
+  }
+}
+</style>
---
+++ src/views/AddressManage/AddAddress.vue
@@ -0,0 +1,85 @@
+<!--  -->
+<template>
+  <div class="edit-address">
+    <all-header>
+      <template #left>
+        <i class="iconfont icon-xiaoyuhao color-fff"></i>
+      </template>
+      <template #center>
+        <span class="color-fff">编辑地址</span>
+      </template>
+    </all-header>
+    <div class="detail">
+      <van-address-edit
+        :area-list="areaList"
+        show-delete
+        show-set-default
+        show-search-result
+        :search-result="searchResult"
+        :area-columns-placeholder="['请选择', '请选择', '请选择']"
+        @save="onSave"
+        @delete="onDelete"
+        @change-detail="onChangeDetail"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import AllHeader from '@/components/AllHeader'
+import { areaList } from '@vant/area-data'
+export default {
+  name: 'AddAddress',
+  data() {
+    return {
+      addressInfo: {},
+      searchResult: [],
+      areaList
+    }
+  },
+  components: { AllHeader },
+  methods: {
+    onSave() {},
+    onDelete() {},
+    onChangeDetail() {}
+  }
+}
+</script>
+<style lang="scss" scoped>
+.edit-address {
+  .detail {
+    ::v-deep .van-address-edit {
+      padding: 0;
+      .van-address-edit__fields {
+        & > .van-cell {
+          padding: 10px;
+        }
+        .van-address-edit-detail .van-field {
+          padding: 0;
+        }
+      }
+      .van-address-edit__default {
+        padding: 10px;
+      }
+    }
+    ::v-deep .van-address-edit__buttons {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .van-button {
+        &:first-child {
+          width: 300px;
+          border-radius: 2px;
+          background-color: #b0352f;
+        }
+        &:last-child {
+          width: 300px;
+          color: #fff;
+          border-radius: 2px;
+          background-color: #000000;
+        }
+      }
+    }
+  }
+}
+</style>
